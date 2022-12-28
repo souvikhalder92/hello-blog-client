662b940db3b1b429d6e4a68e4858500bfbe9eb99
mdshimul101 <shimulfjr101@gmail.com>
2022-12-29 01:16:13 +0600

    shimul from souvik

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    <p>Souvik's Room</p>
-    <h1>Souvikdddddd</h1>
-    <p>hhiff</p>
-=======
+      <p>Souvik's Room</p>
+      <h1>Souvikdddddd</h1>
+      <p>hhiff</p>
       <p>Souvik</p>
       <p>This is ok , I am from Shimul branch</p>
       <p>This is new</p>
       <h1>hello </h1>
->>>>>>> origin/shimul
     </div>
   );
 }
