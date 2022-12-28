4b90dc37b4e5af4a0679d6bf2257487711e326ce
mdshimul101 <shimulfjr101@gmail.com>
2022-12-29 00:55:09 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-    <p>Souvik's Room</p>
-<<<<<<< HEAD
+      <p>Souvik's Room</p>
       <p>Souvik</p>
       <p>This is ok , I am from Shimul branch</p>
       <p>This is new</p>
       <h1>hello </h1>
-=======
-    <h1>Souvikdddddd</h1>
-    <p>hhiff</p>
->>>>>>> souvik
+      <h1>Souvikdddddd</h1>
+      <p>hhiff</p>
     </div>
   );
 }
