9a1d139dab489658add9ed509abbcf68868105db
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-12-29 20:49:28 +0600

    tailwind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <p>Souvik's Room</p>
       <p>Souvik</p>
       <p>This is ok , I am from Shimul branch</p>
-      <p>This is new</p>
+      <p className="p-9">This is new</p>
       <h1>hello </h1>
     <h1>Souvikdddddd</h1>
     <p>hhiff</p>
