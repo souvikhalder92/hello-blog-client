e3ae8ebaff1c5d68f7498e9e0c0ae0e994b4a69d
mdshimul101 <shimulfjr101@gmail.com>
2022-12-29 21:07:31 +0600

    okkkkk

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ function App() {
   return (
     <div className="App">
       <p>Souvik's Room</p>
-<<<<<<< HEAD
-=======
       <h1>Souvikdddddd</h1>
       <p>hhiff</p>
       <p>Souvik's Room</p>
       <h1>Souvikdddddd</h1>
       <p>hhiff</p>
->>>>>>> shimul
       <p>Souvik</p>
       <p>This is ok , I am from Shimul branch</p>
       <p className="p-9">This is new</p>
