351fdea0e118936feec45c1b7fd14eadb564a86f
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-12-29 20:58:35 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ function App() {
   return (
     <div className="App">
     <p>Souvik's Room</p>
-<<<<<<< HEAD
     <h1>Souvikdddddd</h1>
     <p>hhiff</p>
-=======
->>>>>>> main
       <p>Souvik</p>
       <p>This is ok , I am from Shimul branch</p>
       <p className="p-9">This is new</p>
       <h1>hello </h1>
-<<<<<<< HEAD
-=======
     <h1>Souvikdddddd</h1>
     <p>hhiff</p>
->>>>>>> main
     </div>
   );
 }
