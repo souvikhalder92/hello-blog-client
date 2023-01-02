92c021f00d0899f91164f71716af1ed5b268bff9
mdshimul101 <shimulfjr101@gmail.com>
2023-01-02 23:38:57 +0600

    change from souvik

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -3,8 +3,8 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <div className="carousel mx-auto">
-        <div className="carousel-item">
+      <div className="carousel">
+        <div className="">
           <img
             src="https://img.freepik.com/premium-photo/woman-works-office-blue-background-concept-workspace-working-computer-freelance-banner_164357-1144.jpg?w=2000"
             alt="Burger"
