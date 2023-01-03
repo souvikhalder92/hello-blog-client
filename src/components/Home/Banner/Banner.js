45d999d56762277a15d6846bb61fb58f0ef23d51
mdshimul101 <shimulfjr101@gmail.com>
2023-01-03 22:46:05 +0600

    add animation and some change

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -5,13 +5,14 @@ const Banner = () => {
     <div>
       <div className="carousel">
         <div className="relative ">
-          <h1 className="underHover underHoverCenter text-[2rem] lg:text-[3rem]  absolute top-[30%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <h1 className="underHover underHoverCenter text-[1.5rem]  lg:text-[3rem]  absolute top-[30%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
             Hello Blog
           </h1>
-          <p className="underHover after:contents-[''] absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <p className="hidden lg:block underHover underHoverLeft  absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
             Look new post or create a article{" "}
           </p>
           <img
+            className=""
             src="https://img.freepik.com/premium-photo/woman-works-office-blue-background-concept-workspace-working-computer-freelance-banner_164357-1144.jpg?w=2000"
             alt="Burger"
           />
